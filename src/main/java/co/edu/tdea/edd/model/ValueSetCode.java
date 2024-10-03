2786486bf25a6b1db2b090f0f3314dac224f69a2
EmmanuelAgudelo <emmanuelagudelo1604@gmail.com>
2024-10-02 20:52:30 -0500

    first commit

---
+++ src/main/java/co/edu/tdea/edd/model/ValueSetCode.java
@@ -0,0 +1,18 @@
+package co.edu.tdea.edd.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ValueSetCode {
+
+    private String code;
+    private String description;
+}
